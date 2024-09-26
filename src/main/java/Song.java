584cce1033514284b375d1c29520883e58cea84c
savosin752@gmail.com <savosin752@gmail.com>
2024-09-26 13:29:42 +0300

    Creat class for Song and Class for work with Song

---
+++ src/main/java/Song.java
@@ -0,0 +1,18 @@
+public class Song {
+    private String path;
+    private String artist;
+    private String name;
+    private String len_of_song;
+
+    // Getters
+    public String getPath() { return path; }
+    public String getArtist() { return artist; }
+    public String getName() { return name; }
+    public String getLen_of_song() { return len_of_song; }
+
+    // Setters
+    public void setPath(String path) { this.path = path; }
+    public void setArtist(String artist) { this.artist = artist; }
+    public void setName(String name) { this.name = name; }
+    public void setLen_of_song(String len_of_song) { this.len_of_song = len_of_song; }
+}
